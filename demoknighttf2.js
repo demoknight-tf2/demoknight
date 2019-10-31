be24cccd8860e9317471ae72059b704729f57de0
demoknight-tf2 <56979323+demoknight-tf2@users.noreply.github.com>
2019-10-31 22:18:38 +0100

    Add files via upload

--- demoknighttf2.js
+++ demoknighttf2.js
@@ -1,6 +1,30 @@
 const Discord = require('discord.js');
+const tmi = require('tmi.js');
 
 const client = new Discord.Client({disableEveryone: true});
+
+const options = {
+	connection: {
+		reconnect: true,
+		secure: true,
+	},
+	options: {
+		debug: true,
+	},
+	identity: {
+		username: "demoknight_tf2",
+		password: "oauth:q7det5pjtc8q6vqfcbtc6vclqlmz8o"
+	},
+	channels: ['SolarLightTF2']
+};
+
+const tclient = new tmi.client(options);
+tclient.connect();
+tclient.on('chat', function(channel, userstate, message, self) {
+	if (message == "!demoknighttf2") {
+		client.say(channel, userstate['display-name'] + " has praised the holy demoknight team fortress 2");
+	}
+});
 
 client.on("ready", function() {
 	client.user.setActivity('demoknight tf2', {type: 'PLAYING'});
